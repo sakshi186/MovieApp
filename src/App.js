608c82706c81140c2e9e911261b2d152eb0ba137
sakshi186 <sakshisisodiya186@gmail.com>
2022-12-29 16:24:19 +0000

    remove from favourite functionality

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
-import './App.css';
+import React ,{useState, useEffect}from 'react';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MovieList from './components/MovieList';
+import MovieListHeading from './components/MovieListHeading';
+import SearchBox from './components/SearchBox';
+import AddFavourites from './components/AddFavourites';
+import RemoveFavourites from './components/RemoveFavourites';
+
+
 
 function App() {
+
+  const[movies,setMovies]=useState([])
+  const[favourites,setFavourites] = useState([])
+  const[searchValue,setSearchValue]=useState("");
+ 
+
+  const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=6c201a1d`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+  const addFavouriteMovie = (movie) => {
+		const newFavouriteList = [...favourites, movie];
+		setFavourites(newFavouriteList);
+	
+	};
+  const removeFavouriteMovie=(movie)=>{
+		const newFavouriteList=favourites.filter(
+			(favourite)=>favourite.imdbID !==movie.imdbID
+		)
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <div className="container-fluid movie-app">
+           <div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Movies' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={movies}
+					handleFavouritesClick={addFavouriteMovie}
+					favouriteComponent={AddFavourites}
+				/>
+			</div>
+			<div className='row d-flex align-items-center mt-4 mb-4'>
+				<MovieListHeading heading='Favourites' />
+			</div>
+			<div className='row'>
+				<MovieList
+					movies={favourites}
+					handleFavouritesClick={removeFavouriteMovie}
+					favouriteComponent={RemoveFavourites}
+					/>
+          </div>
+        </div>
+          
   );
 }
 
